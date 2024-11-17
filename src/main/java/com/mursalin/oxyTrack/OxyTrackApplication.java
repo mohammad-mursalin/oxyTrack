b8643e3a45960590af25ba393aab1056ea3b742b
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-17 09:23:27 +0600

    initial comment

---
+++ src/main/java/com/mursalin/oxyTrack/OxyTrackApplication.java
@@ -0,0 +1,13 @@
+package com.mursalin.oxyTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxyTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxyTrackApplication.class, args);
+	}
+
+}
