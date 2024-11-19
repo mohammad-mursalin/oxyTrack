706268078c98045081770f7df15deb0a2f87e411
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-19 23:26:42 +0600

    origin updated

--- src/main/java/com/mursalin/oxyTrack/controller/OxyDataController.java
+++ src/main/java/com/mursalin/oxyTrack/controller/OxyDataController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/oxyTrack")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:5173/")
 public class OxyDataController {
 
     private OxyService service;
