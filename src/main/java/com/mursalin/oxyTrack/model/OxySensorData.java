c0da9b209572bb74c975f4aba94f68f1c250bf26
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-23 12:09:07 +0600

    repository added

--- src/main/java/com/mursalin/oxyTrack/model/OxySensorData.java
+++ src/main/java/com/mursalin/oxyTrack/model/OxySensorData.java
@@ -1,5 +1,8 @@
 package com.mursalin.oxyTrack.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class OxySensorData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String deviceId;
     private LocalDateTime timestamp;
     private double oxygenLevel;
--- src/main/java/com/mursalin/oxyTrack/repository/OxyRepository.java
+++ src/main/java/com/mursalin/oxyTrack/repository/OxyRepository.java
@@ -1,5 +1,10 @@
 package com.mursalin.oxyTrack.repository;
 
-public interface OxyRepository {
+import com.mursalin.oxyTrack.model.OxySensorData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OxyRepository extends JpaRepository<OxySensorData, Long> {
 
 }
